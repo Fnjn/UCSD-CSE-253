db3eca0f6ae8ec6ebc59adf3bc3b7a7854066e23
plrectco <lightbulb@sjtu.edu.cn>
2018-01-15 00:48:45 -0800

    Several updates 1. tex: 3 and part of 5 2. softmax to fulfill a b c, except d

--- hw1/Softmax-Regression-MNIST.py
+++ hw1/Softmax-Regression-MNIST.py
@@ -18,6 +18,14 @@ print(test_Y.shape) # 10 * m_test
 print(w.shape)  # 10 * n_feature
 print(b.shape)  # 10 * 1
 '''
+def compute_cost(X, Y, w, b, lambd, regularized=2):
+    m = Y.shape[-1]
+    Z = np.dot(w, X) + b
+    A = softmax(Z)
+    cost = - np.sum(Y * np.log(A)) / m
+    cost += lambd * np.linalg.norm(w) / m  # L2 Regularization
+
+    return cost
 
 def softmax_propagate(X, Y, w, b, lambd):
     m = Y.shape[-1]
@@ -90,15 +98,40 @@ class SoftmaxRegression(object):
         self.parameters = {'w':w, 'b':b, 'v':v, 's':s}
         self.cost = -1.
 
-    def fit(self, X, Y):
+    def fit(self, X, Y, test_X=None, test_Y=None, holdout=0.1):
+        train = {'cost': [], 'accuracy': []}
+        val = {'cost': [], 'accuracy':[]}
+        test = {'cost':[], 'accuracy':[]}
+
+        m = X.shape[-1]
+        permutation = np.random.permutation(m)
+        X_shuffle = X[:, permutation]
+        Y_shuffle = Y[:, permutation]
+        m_holdout = int(m*holdout)
+
+
         for i in range(self.n_epoch):
-            X_batches, Y_batches, n_batch = create_batch(X, Y, self.batch_size)
+            X_batches, Y_batches, n_batch = create_batch(X[:, m_holdout:], Y[:, m_holdout:], self.batch_size)
 
             for j in range(n_batch):
                 self.parameters, self.cost = adam_optimize(X_batches[j], Y_batches[j], self.parameters, \
                 self.learning_rate, self.lambd, self.beta1, self.beta2, self.epsilon)
 
-            if i % 20 == 0: print('%d epoches cost: %f' % (i, self.cost))
+
+            if i % 20 == 0:
+                print('%d epoches cost: %f' % (i, self.cost))
+
+                # Recording data for plotting
+                train['cost'].append(self.cost)
+                val['cost'].append(compute_cost(X[:, :m_holdout], Y[:, :m_holdout], self.parameters['w'], self.parameters['b'], self.lambd))
+                train['accuracy'].append(self.predict(X[:, m_holdout:], Y[:, m_holdout:]))
+                val['accuracy'].append(self.predict(X[:, :m_holdout], Y[:, :m_holdout]))
+                if test_X is not None and test_Y is not None:
+                    test['cost'].append(compute_cost(test_X, test_Y, self.parameters['w'], self.parameters['b'], self.lambd))
+                    test['accuracy'].append(self.predict(test_X, test_Y))
+
+        return train, val, test
+
 
     def predict(self, X, Y):
         m = X.shape[-1]
@@ -109,8 +142,12 @@ class SoftmaxRegression(object):
         A = softmax(Z)
 
         self.Y_p = np.argmax(A, axis=0)
-        correct = (self.Y_p == Y)
-        self.accuracy = np.sum(correct)*1.0 / m
+        Y_label = np.argmax(Y, axis=0)
+        correct = (self.Y_p == Y_label)
+
+        self.accuracy = np.sum(correct)/ m
+
+        return self.accuracy
 
 
 def __main__():
@@ -140,10 +177,37 @@ def __main__():
 
     n_feature = train_X.shape[0]
     n_classes = 10
+    #
+    # softmax_model = SoftmaxRegression(n_feature, n_classes, n_epoch=400)
+    # softmax_model.fit(train_X, train_Y)
+    # softmax_model.predict(test_X, test_labels)
+    # print('Softmax Regression Accuracy: %f %%' % (softmax_model.accuracy * 100))
 
     softmax_model = SoftmaxRegression(n_feature, n_classes, n_epoch=400)
-    softmax_model.fit(train_X, train_Y)
-    softmax_model.predict(test_X, test_labels)
+    train, val, test = softmax_model.fit(train_X, train_Y, test_X=test_X, test_Y=test_Y)
+    softmax_model.predict(test_X, test_Y)
     print('Softmax Regression Accuracy: %f %%' % (softmax_model.accuracy * 100))
+
+    fig1 = plt.figure()
+    ax1 = fig1.add_subplot(211)
+    tc_plt, = ax1.plot(train['cost'], label='Training Cost')
+    vc_plt, = ax1.plot(val['cost'], label='Validation Cost')
+    tec_plt, =  ax1.plot(test['cost'], label='Test Cost')
+    ax1.legend(handles=[tc_plt, vc_plt, tec_plt])
+    ax2 = fig1.add_subplot(212)
+    ta_plt, = ax2.plot(train['accuracy'], label='Training Accuracy')
+    va_plt, = ax2.plot(val['accuracy'], label='Validating Accuracy')
+    tea_plt, =  ax2.plot(test['accuracy'], label='Test Accuracy')
+    ax2.legend(handles=[ta_plt, va_plt, tea_plt], loc=4)
+    plt.show()
+
+
+    fig2 = plt.figure()
+    plt.gray()
+
+    for i in range(10):
+        ax3 = fig2.add_subplot(2, 5, i+1)
+        ax3.imshow(softmax_model.parameters['w'][i].reshape(28,28))
+    plt.show()
 
 __main__()
