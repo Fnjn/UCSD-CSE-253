821c1e848759a05dc6f5c6622d5134fcee6e17e6
qinacme <qinhehahaha@hotmail.com>
2018-02-12 16:35:28 -0800

    [qinacme]: Update model.

--- hw3/cifar10_models_qinacme.py
+++ hw3/cifar10_models_qinacme.py
@@ -27,13 +27,13 @@ def conv_bn_relu(in_channels, out_channels, kernel_size, padding=1, init="xavier
 class model_torch_official(nn.Module):
     def __init__(self):
         super().__init__()
-        self.conv1 = conv_bn_relu(3, 6, 5, pooling=True)
-        self.conv2 = conv_bn_relu(6, 16, 5, pooling=True)
+        self.conv1 = conv_bn_relu(3, 16, 5, pooling=True)
+        self.conv2 = conv_bn_relu(16, 64, 5, pooling=True)
         
 #         self.conv1 = nn.Conv2d(3,6,5)
 #         self.pool = nn.MaxPool2d(2,2)
 #         self.conv2 = nn.Conv2d(6,16,5)
-        self.fc1 = nn.Linear(16*5*5, 120)
+        self.fc1 = nn.Linear(64*5*5, 120)
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, 10)
         
@@ -42,7 +42,7 @@ class model_torch_official(nn.Module):
 #         x = self.pool(F.relu(self.conv2(x)))
         x = self.conv1(x)
         x = self.conv2(x)
-        x = x.view(-1, 16*5*5)
+        x = x.view(-1, 64*5*5)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
