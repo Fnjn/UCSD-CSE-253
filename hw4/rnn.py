bb09e5c6f250a970d720f269817916e4919efebb
Fanjin <fjzeng@ucsd.edu>
2018-02-26 04:57:00 -0800

    trivial

--- hw4/rnn.py
+++ hw4/rnn.py
@@ -11,23 +11,28 @@ import numpy as np
 
 class RNN(nn.Module):
 
-    def __init__(self, input_dim, hidden_dim, tagset_size):
+    def __init__(self, input_dim, hidden_dim, num_layers, output_size):
         super().__init__()
         self.input_dim = input_dim
         self.hidden_dim = hidden_dim
-        self.lstm1 = nn.LSTM(input_dim, hidden_dim, num_layers=3)
-        self.hidden2tag = nn.Linear(hidden_dim, tagset_size)
+        self.num_layers = num_layers
+
+        self.input2hidden = nn.Linear(input_dim, hidden_dim)
+        self.lstm = nn.LSTM(hidden_dim, hidden_dim, num_layers=num_layers, dropout=0.05)
+        self.hidden2tag = nn.Linear(hidden_dim, output_size)
         self.hidden = self.init_hidden()
 
     def init_hidden(self):
-        h = torch.zeros(3,1,self.hidden_dim)
-        c = torch.zeros(3,1,self.hidden_dim)
+        h = torch.zeros(self.num_layers,1,self.hidden_dim)
+        c = torch.zeros(self.num_layers,1,self.hidden_dim)
         return (Variable(h.cuda()), Variable(c.cuda()))
 
-    def forward(self, sentence):
-        lstm_out, self.hidden = self.lstm(sentence.view(len(sentence), 1, -1), self.hidden)
-        tag_scores = self.hidden2tag(lstm_out.view(len(sentence), -1))
-        return tag_scores
+    def forward(self, x):
+        x_len = len(x)
+        x = self.input2hidden(x)
+        x, self.hidden = self.lstm(x.view(x_len, 1, -1), self.hidden)
+        x = self.hidden2tag(x.view(x_len, -1))
+        return x
 
 
 class PrepareData():
@@ -43,7 +48,11 @@ class PrepareData():
         with open(filename, 'r') as f:
             data = f.read()
         self.data = [data[i*batch_size:(i+1)*batch_size] for i in range(len(data)//batch_size)]
-        self.shuffle_data()
+
+        n_data = len(self.data)
+        n_val = int(n_data * self.val_ratio)
+        self.train_data = self.data[n_val:]
+        self.val_data = self.data[:n_val]
 
     def shuffle_data(self):
         n_data = len(self.data)
